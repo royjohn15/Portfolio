55085b460adbbd0dc38c5e31eced60875e9d0650
Roy John <royjohnabc@gmail.com>
2023-02-21 01:23:53 +0530

    Fixed the dots issue

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,50 @@
-const container = document.querySelector('.dot-container');
-const DOT_COUNT = 50;
-const THRESHOLD_DISTANCE = 100;
+// Define a constant for the maximum distance at which the dots should move away from the mouse
+const MAX_DISTANCE = 40;
 
-function createDot() {
-  const dot = document.createElement('div');
-  dot.classList.add('dot');
-  container.appendChild(dot);
-
-  const x = Math.floor(Math.random() * window.innerWidth);
-  const y = Math.floor(Math.random() * window.innerHeight);
-  dot.style.transform = `translate(${x}px, ${y}px)`;
-  
-  dot.initialX = x;
-  dot.initialY = y;
+// Define a function to create the dots
+function createDots(numDots) {
+  for (let i = 0; i < numDots; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    dot.style.top = Math.random() * window.innerHeight + 'px';
+    dot.style.left = Math.random() * window.innerWidth + 'px';
+    document.body.appendChild(dot);
+  }
 }
 
-for (let i = 0; i < DOT_COUNT; i++) {
-  createDot();
-}
+// Call the createDots function with a number of dots to create
+createDots(90);
 
-container.addEventListener('mousemove', e => {
+// Add an event listener for the mousemove event on the document
+document.addEventListener('mousemove', (e) => {
+  // Get a NodeList of all the dots
   const dots = document.querySelectorAll('.dot');
-  dots.forEach(dot => {
-    const dx = dot.offsetLeft - e.clientX;
-    const dy = dot.offsetTop - e.clientY;
-    const dist = Math.sqrt(dx * dx + dy * dy);
-    if (dist < THRESHOLD_DISTANCE) {
-      const angle = Math.atan2(dy, dx);
-      const tx = e.clientX + Math.cos(angle) * THRESHOLD_DISTANCE;
-      const ty = e.clientY + Math.sin(angle) * THRESHOLD_DISTANCE;
-      dot.style.transform = `translate(${tx}px, ${ty}px)`;
-    } else {
-      dot.style.transform = `translate(${dot.initialX}px, ${dot.initialY}px)`;
+
+  // Loop through each dot
+  dots.forEach((dot) => {
+    // Calculate the distance between the mouse and the dot
+    const dx = e.clientX - dot.offsetLeft;
+    const dy = e.clientY - dot.offsetTop;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // If the distance is less than the maximum distance, move the dot away from the mouse
+    if (distance < MAX_DISTANCE) {
+      const x = dot.offsetLeft + dx / distance * MAX_DISTANCE * 0.1;
+      const y = dot.offsetTop + dy / distance * MAX_DISTANCE * 0.1;
+      dot.style.left = x + 'px';
+      dot.style.top = y + 'px';
+    } 
+    // Otherwise, reset the position of the dot to its initial position
+    else {
+      dot.style.top = dot.initialTop;
+      dot.style.left = dot.initialLeft;
     }
   });
+});
+
+// Store the initial position of each dot as a property on the dot element
+const dots = document.querySelectorAll('.dot');
+dots.forEach((dot) => {
+  dot.initialTop = dot.style.top;
+  dot.initialLeft = dot.style.left;
 });
