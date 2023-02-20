615f49182eca8de2b4f1d4667a4747a3c57065e8
Roy John <royjohnabc@gmail.com>
2023-02-21 00:38:50 +0530

    Add new files

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,37 @@
+const container = document.querySelector('.dot-container');
+const DOT_COUNT = 50;
+const THRESHOLD_DISTANCE = 100;
 
+function createDot() {
+  const dot = document.createElement('div');
+  dot.classList.add('dot');
+  container.appendChild(dot);
+
+  const x = Math.floor(Math.random() * window.innerWidth);
+  const y = Math.floor(Math.random() * window.innerHeight);
+  dot.style.transform = `translate(${x}px, ${y}px)`;
+  
+  dot.initialX = x;
+  dot.initialY = y;
+}
+
+for (let i = 0; i < DOT_COUNT; i++) {
+  createDot();
+}
+
+container.addEventListener('mousemove', e => {
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach(dot => {
+    const dx = dot.offsetLeft - e.clientX;
+    const dy = dot.offsetTop - e.clientY;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < THRESHOLD_DISTANCE) {
+      const angle = Math.atan2(dy, dx);
+      const tx = e.clientX + Math.cos(angle) * THRESHOLD_DISTANCE;
+      const ty = e.clientY + Math.sin(angle) * THRESHOLD_DISTANCE;
+      dot.style.transform = `translate(${tx}px, ${ty}px)`;
+    } else {
+      dot.style.transform = `translate(${dot.initialX}px, ${dot.initialY}px)`;
+    }
+  });
+});
